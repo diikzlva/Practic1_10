ce6e763f6fa22227e2a4c1820012a1e87ffbae0f
Динара <@LAPTOP-EV6I0PGS>
2024-03-29 01:17:07 +0300

    3 commit

--- src/practical_1_10/Practical_1_10.java
+++ src/practical_1_10/Practical_1_10.java
@@ -13,22 +13,20 @@ public class Practical_1_10 {
        
        Scanner scanner = new Scanner(System.in);
         
-        // Путь к исходному файлу
-        System.out.println("Введите путь к исходному файлу: ");
+        System.out.println("Введите полный путь к исходному файлу: ");
         String inputFilePath = scanner.nextLine();
 
-        // Проверка существования файла
         File inputFile = new File(inputFilePath);
         if (!inputFile.exists()) {
             System.out.println("Файл не найден");
             return;
         }
-        // Чтение строк из файла
+
         StringBuilder output = new StringBuilder();
         try (Scanner fileScanner = new Scanner(inputFile)) {
             while (fileScanner.hasNextLine()) {
                 String line = fileScanner.nextLine();
-                // Переворачиваем строку
+        
                 StringBuilder reversedLine = new StringBuilder();
                 for (int i = line.length() - 1; i >= 0; i--) {
                     reversedLine.append(line.charAt(i));
@@ -37,20 +35,16 @@ public class Practical_1_10 {
             }
         } catch (FileNotFoundException e) {
             System.out.println("Ошибка при чтении файла!");
-            e.printStackTrace();
             return;
         }
 
-        // Путь к новому файлу
         String outputFilePath = inputFilePath.substring(0, inputFilePath.lastIndexOf('.')) + "_reversed.txt";
 
-        // Запись перевернутых строк в новый файл
         try (FileWriter fileWriter = new FileWriter(outputFilePath)) {
             fileWriter.write(output.toString());
             System.out.println("Файл успешно модифицирован. Перевернутые строки сохранены в файле: " + outputFilePath);
         } catch (IOException e) {
             System.out.println("Ошибка при работе с файлами: " + e.getMessage());
-            e.printStackTrace();
         }
     }
 }
